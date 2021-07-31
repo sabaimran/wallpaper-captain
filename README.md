4721670170f1248417a25b1a1319489abfaa22d5
Saba <narmiabas@gmail.com>
2021-07-31 14:24:29 -0700

    Add the wallpaper-updater and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Wallpaper Captain!
+I want to use wallpapers that mean something to me. At first iteration, I'll set the wallpaper to a random picture from a folder in which I keep photos from various occasions in my life. Each random photo allows me to think back to a different time, state, or place I was in, and to be serendipitously reminded of its beauty.
+
+## Pre-execution Setup
+Before running the script, you must set the relevant environment variable:
+`export WALLPAPER_CAPTAIN_FOLDER=/path/to/wallpapers/folder/`
+
+## System
+Run with Python 3.9.5 on Ubuntu 21.04
---
+++ wallpaper-updater.py
@@ -0,0 +1,24 @@
+import os
+from random import randrange
+from os.path import isfile, join
+
+# This indicates where the images used for the wallpaper are stored.
+wallpaper_folder_path = os.environ['WALLPAPER_CAPTAIN_FOLDER']
+
+def set_wallpaper(filepath: str):
+    """
+    Given a filepath to an image, sets the wallpaper to that image.
+    """
+    os.system(f"gsettings set org.gnome.desktop.background picture-uri file:{filepath}")
+
+def get_wallpaper_options():
+    """
+    Return the list of filepaths to wallpaper options.
+    """
+    return [join(wallpaper_folder_path, f) for f in os.listdir(wallpaper_folder_path)]
+
+if __name__ == "__main__":
+    wallpaper_options = get_wallpaper_options()
+
+    # Pick a random index in the range of the number of wallpaper options, and set the background.
+    set_wallpaper(wallpaper_options[randrange(len(wallpaper_options))])
