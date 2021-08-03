1c304ecf25c5038382a9a0640223a81f702b1e6b
Saba <narmiabas@gmail.com>
2021-08-03 16:18:49 -0700

    Addresses #2 in order to have an at max once per day schedule

--- sample-update.sh
+++ sample-update.sh
@@ -1,7 +1,23 @@
 #! /bin/sh
 
-# This is a sample file to get started with scheduling a cronjob for updating your wallpaper using the Python script.
-echo "starting: $(date)"
+# This is a sample file to get started with scheduling a cronjob for updating your wallpaper using the Python script. 
+# In your crontab, there should be an entry as follows:
+# @hourly /path/to/sample-update.sh >> /path/to/output.log 2>&1
+
+# Determine the last execution time based on the output log and convert it to a date object. We are doing this because the cronjob is not guaranteed to run daily in case the computer is asleep during the scheduled time. Hence, we have set it to run hourly, and check against our logfile to deem if it is actually eligible to update the wallpaper.
+last_execution_time=$(tail -1 path/to/output.log)
+last_execution_time=$(date -d "$last_execution_time" +%s)
+
+# Determine at most how recently the script could have been run last in order to be eligible. If it has been run too recently, then exit the program.
+max_time_next_execution=$(date -d "-1 days" +%s)
+
+if [ $last_execution_time -ge $max_time_next_execution ];
+then
+    exit 0
+fi
+
+# We will change the wallpaper! Log the date in the log file to keep track of the last run.
+echo "$(date)"
 
 # Replace with path to your wallpapers folder
 export WALLPAPER_CAPTAIN_FOLDER=/path/to/your/wallpapers/
